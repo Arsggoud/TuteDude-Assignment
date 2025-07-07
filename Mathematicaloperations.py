6b82fbd377ba40db3af164ede4e3945137da6f00
Arsggoud <mrahdgaud@gmail.com>
2025-07-07 16:04:04 +0530

    Add files via upload

---
+++ Mathematicaloperations.py
@@ -0,0 +1,9 @@
+a = input("Please enter your first number --> ")
+b = input("Please enter your second number --> ")
+a = float(a)
+b = float(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
